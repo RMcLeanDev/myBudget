7648f644013d1fd3094eda8273e00eee56497293
Lilkgb <mcleanrs@comcast.net>
2020-01-10 17:30:17 -0800

    add signup component and put inputs and signup function that creates a account

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,11 +1,14 @@
 import React, {useState} from "react";
 import * as firebase from 'firebase';
+import SignUp from './SignUp';
 
 function SignIn(){
 
     const [error, setError] = useState(null);
+    const [signUp, setSignUp] = useState(false);
     let _email = null;
     let _password = null;
+    let signUpForm = null;
 
     function signInFunction(e){
         e.preventDefault();
@@ -22,14 +25,22 @@ function SignIn(){
         firebase.auth().signOut();
     }
 
+    if(signUp){
+        signUpForm = <SignUp closeSignUp={() => {setSignUp(false)}}/>
+    } else {
+        signUpForm = null;
+    }
+
     return(
-        <div>
+        <div className="signIn">
+            {signUpForm}
             <form onSubmit={signInFunction}>
                 <input placeholder="Email" type="email" ref={value => {_email = value}}/>
                 <input placeholder="Password" type="password" ref={value => {_password = value}}/>
                 <button type="submit">Sign in</button>
                 <p>{error}</p>
             </form>
+            <h3 onClick={() => {setSignUp(true)}}>Create account?</h3>
             <button onClick={signOut}>Sign Out</button>
         </div>
     )
---
+++ src/components/SignUp.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import * as firebase from 'firebase/app';
+
+function SignUp(props){
+
+    const [passwordError, setPasswordError] = useState(false);
+
+    let _name = null;
+    let _email = null;
+    let _passwordOne = null;
+    let _passwordTwo = null;
+    let _botProtection = null;
+
+    function signUpFunction(e){
+        e.preventDefault();
+        if(_botProtection.value === ''){
+            if(_passwordOne.value !== _passwordTwo.value){
+                _passwordOne.value = '';
+                _passwordTwo.value = '';
+                return setPasswordError("Your passwords doesn't match. Please try again.")
+            } else {
+                firebase.auth().createUserWithEmailAndPassword(_email.value, _passwordOne.value).then(user => {
+                    firebase.database().ref(`users/${user.user.uid}`).set({name: _name.value,id: user.user.uid})
+                }).catch(error => {
+                    setPasswordError(error.message)
+                })
+            }
+        } else {
+            console.log('no')
+        }
+    }
+
+    return(
+        <div className="signUpForm">
+            <form onSubmit={signUpFunction}>
+                <input type="text" placeholder='Name' ref={value => {_name = value}}/><br />
+                <input type="email" placeholder='Email' ref={value => {_email = value}}/><br />
+                <input type="password" placeholder="Password" ref={value => {_passwordOne = value}}/><br />
+                <input type="password" placeholder="Confirm Password" ref={value => {_passwordTwo = value}}/><br />
+                {passwordError}
+                <input placeholder="bot" style={{display: "none"}} ref={value => {_botProtection = value}} />
+                <br />
+                <button type="submit">Sign Up</button>
+            </form>
+            <p>Already have a account? <span onClick={props.closeSignUp}>Click Here!</span></p>
+        </div>
+    )
+}
+
+export default SignUp;
