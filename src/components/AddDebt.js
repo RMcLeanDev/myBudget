0803272fd58f1ccbde5be2100160864822db1299
Lilkgb <mcleanrs@comcast.net>
2020-02-11 20:28:16 -0800

    add interest button on add debt for and work on pmt function to calculate interest

--- src/components/AddDebt.js
+++ src/components/AddDebt.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as firebase from 'firebase';
 import { v4 } from 'uuid';
 import '../scss/AddDebts.scss';
 
 function AddDebt(props){
 
+    const [checkInterest, setInterest] = useState(false);
+
     let _total = null;
     let _current = null;
     let _name = null;
+    let _air = null;
+    let _years = null;
+    let _ppy = null;
+    let _amount = null;
+    let checkedInterest;
 
     function sendDebt(e){
         e.preventDefault()
@@ -31,14 +38,49 @@ function AddDebt(props){
         _name.value = null;
     }
 
+    function pmt(e){
+        e.preventDefault();
+        // interest is (interest_rate/12 * balance);
+        let r = parseFloat((_air.value / 100) / 12);
+        let months = parseInt(_years.value * 12);
+        let payment = parseFloat(_amount.value) * ((r * Math.pow(1 + r, months)) / (Math.pow(1 + r, months) - 1));
+        console.log(payment.toFixed(2));
+    }
+
+    if(checkInterest){
+        checkedInterest = <div>
+            <input placeholder="Annual Interest Rate" ref={value => {_air = value}}/>
+            <input placeholder="Years" ref={value => {_years = value}}/>
+            <input placeholder="Payments Per Year" ref={value => {_ppy = value}}/>
+        </div>
+    } else {
+        checkedInterest = null;
+    }
+
+    function interestBox(){
+        if(checkInterest){
+            setInterest(false)
+        } else {
+            setInterest(true)
+        }
+    }
+
     return(
         <div className="addDebtContainer">
             <img src={require("../assets/x.png")} onClick={props.closeDebtForm}/>
             <div className="addDebtWindow">
+                <div className="interestCheck">
+                    <label class="checkbox-label">
+                        <input type="checkbox" checked={checkInterest} onClick={interestBox}/>
+                        <span class="checkbox-custom rectangular"></span>
+                    </label>
+                    <p>Interest</p>
+                </div>
                 <form onSubmit={sendDebt}>
                     <input placeholder="Name of Debt" ref={value => {_name = value}}/>
                     <input type="number" min="1" step="any" placeholder="Total Debt Amount" ref={value => {_total = value}}/>
                     <input type="number" min="0" step="any" placeholder="Current Amount Paid" ref={value => {_current = value}}/>
+                    {checkedInterest}
                     <button type="submit">Enter</button>
                 </form>
             </div>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as firebase from 'firebase';
+import { _instanceWithOptions } from 'firebase-functions/lib/providers/database';
 
 function Budget(){
+
+    const [payments, setPayments] = useState(null)
+
+    console.log(payments);
 
     let _info = null;
 
@@ -14,6 +19,7 @@ function Budget(){
         <div>
             <hr/>
             <h1>Budget Section</h1>
+            <hr />
             <form onSubmit={sendBudget}>
                 <input placeholder="some Information here" ref={value => {_info = value}}/>
                 <button type="submit">Enter</button>
