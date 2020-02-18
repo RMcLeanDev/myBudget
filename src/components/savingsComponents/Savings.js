88cf1409db4277bb00428a4f2fc57270203b7b6b
Lilkgb <mcleanrs@comcast.net>
2020-02-18 12:52:12 -0800

    add div .top from budget to savings and add same styling

--- src/components/savingsComponents/Savings.js
+++ src/components/savingsComponents/Savings.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import '../../scss/Savings.scss';
 
-function Savings(){
+function Savings(props){
+
+    let display;
+    let showHide;
+
+    const [showSavings, setShowSavings] = useState(false);
+    const [addSavingsForm, setSavingsForm] = useState(false);
+
+    if(props.savings){
+        display = <h3>true</h3>
+    } else {
+        display = <h3>Add your first savings item!</h3>
+    }
+
+    if(showSavings){
+        showHide = <h2 onClick={() => setShowSavings(false)}>Hide</h2>
+    } else {
+        showHide = <h2 onClick={() => setShowSavings(true)}>Show</h2>
+    }
+
     return (
-        <h1>Savings Component</h1>
+        <div>
+            <div className="top">
+                {showHide}
+                <h1>Savings</h1>
+                <img onClick={() => setSavingsForm(true)} className="addDebtButton" src={require('../../assets/plus.png')}/>
+            </div>
+            {display}
+        </div>
     )
 }
 
