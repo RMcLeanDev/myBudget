ed14775116e101121499083710bca50c9cbe4d82
Ryan <ryan@MEME-TIME.local>
2022-04-20 09:14:17 -0700

    styling sign in component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,27 @@
 import React, {useState} from 'react';
 
 function SignIn(){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function signIn(e){
+        e.preventDefault()
+        console.log(email, password)
+    }
     return(
-        <div>
-            <h1>Sign in component</h1>
+        <div className="signInComponent">
+            <div className="mainBorder">
+                <h1>Sign in</h1>
+                <form onSubmit={signIn}>
+                    <input placeholder="Email" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <br/>
+                    <button type="submit">Lets Go</button>
+                </form>
+                <hr/>
+                <p>Create an Account!</p>
+            </div>
         </div>
     )
 }
