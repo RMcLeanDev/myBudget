0b983ad8f042863d7b243d8ddf35188ab79ce54c
Ryan <ryan@MEME-TIME.local>
2022-04-22 08:56:15 -0700

    add eye to hide and show password

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,6 +7,7 @@ function SignUp(){
     const [email, setEmail] = useState("")
     const [pass1, setPass1] = useState("")
     const [pass2, setPass2] = useState("")
+    const [viewPass, setViewPass] = useState(true)
 
     function createAccount(e){
         e.preventDefault()
@@ -19,8 +20,9 @@ function SignUp(){
                 <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="First Name"/>
                 <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Last Name"/>
                 <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-                <input type="password" value={pass1} onChange={e => setPass1(e.target.value)} placeholder="Password" />
-                <input type="password" value={pass2} onChange={e => setPass2(e.target.value)} placeholder="Verify Password" />
+                <input type={viewPass ? "password":"text"} value={pass1} onChange={e => setPass1(e.target.value)} placeholder="Password" />
+                <input type={viewPass ? "password":"text"} value={pass2} onChange={e => setPass2(e.target.value)} placeholder="Verify Password" />
+                <i className={viewPass ? "far fa-eye-slash": "far fa-eye"} id="togglePassword" onClick={() => setViewPass(!viewPass)}></i>
                 {pass1 === pass2 ? "":<p>Passwords don't match</p>}
                 <button className="signButton">Create!</button>
             </form>
