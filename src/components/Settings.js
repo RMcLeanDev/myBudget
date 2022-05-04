6c5f9b033acd4759c20c0cc9bccb2dd95de91c8f
Ryan <ryan@MEME-TIME.local>
2022-05-04 13:10:56 -0700

    test addundread function to properly update how many unread messages there are

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/compat/app';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import "firebase/auth";
+import "firebase/compat/database";
 
 function Settings(props){
 
@@ -15,9 +16,19 @@ function Settings(props){
         navigate("/")
     }
 
+    function addUndread(){
+        firebase.database().ref(`users/${process.env.REACT_APP_DEVELOPER_ID}`).once("value", function(snapshot){
+            let info = snapshot.val();
+            let newNum = info.messages.unreadMessages + 1;
+            console.log(newNum)
+            firebase.database().ref(`users/${process.env.REACT_APP_DEVELOPER_ID}/messages`).update({unreadMessages: newNum})
+        })
+    }
+
     return(
         <div>
             <div className="toMessages">
+                <button onClick={addUndread}>Click me</button>
                 <Link to='/messages'>
                     <i class="fa-solid fa-inbox"></i>
                     <p>Message Devloper</p>
